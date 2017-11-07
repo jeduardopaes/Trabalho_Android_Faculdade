d1e70f0e9f2e4555e0564b50857e624ff7a4d09b
Jose E. P. Macedo <j.eduardopaes@gmail.com>
2017-11-07 20:03:09 -0200

    Adiciona variáveis na classe do cadastro de vacinas.

--- app/src/main/java/br/com/vaciprev/vaciperv/CadastroDeVacinas.java
+++ app/src/main/java/br/com/vaciprev/vaciperv/CadastroDeVacinas.java
@@ -1,23 +1,43 @@
 package br.com.vaciprev.vaciperv;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
 
 
 public class CadastroDeVacinas extends AppCompatActivity {
 
-
+    TextInputLayout IDVacina;
+    TextInputLayout DataDaVacina;
+    Switch SegundaDose;
+    Button Salvar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_de_vacinas);
 
+        IDVacina = (TextInputLayout) findViewById(R.id.IDVacina);
+        DataDaVacina = (TextInputLayout) findViewById(R.id.DataDaVacina);
+        SegundaDose = (Switch) findViewById(R.id.SegundaDose);
 
+        Salvar = (Button) findViewById(R.id.Salvar);
+
+        Salvar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CadastroDeVacinas.this, IDVacina.getEditText().getText().toString()+ " "+DataDaVacina.getEditText().getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
+
+
 
 
 }
