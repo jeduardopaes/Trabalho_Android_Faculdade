62fb455bc7e0324ed252d5239250f6bc3640f7df
Jose E. P. Macedo <j.eduardopaes@gmail.com>
2017-11-09 01:29:14 -0200

    Coloca date picker na tela de cadastro.

--- app/src/main/java/br/com/vaciprev/vaciperv/CadastroDeVacinas.java
+++ app/src/main/java/br/com/vaciprev/vaciperv/CadastroDeVacinas.java
@@ -2,18 +2,24 @@ package br.com.vaciprev.vaciperv;
 
 import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.Toast;
 
 
-public class CadastroDeVacinas extends AppCompatActivity {
+public class CadastroDeVacinas extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     TextInputLayout IDVacina;
-    TextInputLayout DataDaVacina;
+    EditText DataDaVacina;
+    EditText DataDaSegundaDose;
     Switch SegundaDose;
     Button Salvar;
 
@@ -23,15 +29,44 @@ public class CadastroDeVacinas extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro_de_vacinas);
 
         IDVacina = (TextInputLayout) findViewById(R.id.IDVacina);
-        DataDaVacina = (TextInputLayout) findViewById(R.id.DataDaVacina);
+        DataDaVacina = (EditText) findViewById(R.id.DataDaVacina);
         SegundaDose = (Switch) findViewById(R.id.SegundaDose);
+        DataDaSegundaDose = (EditText) findViewById(R.id.DataSegundaDose);
+
 
         Salvar = (Button) findViewById(R.id.Salvar);
+
+        DataDaSegundaDose.setVisibility(View.INVISIBLE);
+
 
         Salvar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Toast.makeText(CadastroDeVacinas.this, IDVacina.getEditText().getText().toString()+ " "+DataDaVacina.getEditText().getText().toString(), Toast.LENGTH_LONG).show();
+                Toast.makeText(CadastroDeVacinas.this,
+                        IDVacina.getEditText().getText().toString()+ " "
+                                +DataDaVacina.getText().toString()+" "
+                        , Toast.LENGTH_LONG).show();
+            }
+        });
+
+        SegundaDose.setOnCheckedChangeListener(this);
+
+
+        DataDaVacina.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    showDatePickerDialog(v);
+                }
+            }
+        });
+
+        DataDaSegundaDose.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    showDatePickerDialog(v);
+                }
             }
         });
 
@@ -39,5 +74,19 @@ public class CadastroDeVacinas extends AppCompatActivity {
 
 
 
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if(SegundaDose.isChecked()){
+            DataDaSegundaDose.setVisibility(View.VISIBLE);
 
+        }else{
+            DataDaSegundaDose.setVisibility(View.INVISIBLE);
+
+        }
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
 }
---
+++ app/src/main/java/br/com/vaciprev/vaciperv/DatePickerFragment.java
@@ -0,0 +1,39 @@
+package br.com.vaciprev.vaciperv;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.Layout;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        EditText data = (EditText) getActivity().findViewById(R.id.DataDaVacina);
+
+        data.setText(day+"/"+month+"/"+year);
+    }
+
+
+}
